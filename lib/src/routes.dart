54df9865c6c99301b0d9ba8befa87d54bee75c10
Adryanne Kelly <adryannekelly123@gmail.com>
2024-11-17 17:49:16 -0300

    feat: add routes to new pages

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,7 +1,8 @@
 import 'package:go_router/go_router.dart';
 import 'package:planning_poker_ifood/src/app/features/auth/presentation/pages/login_page.dart';
 import 'package:planning_poker_ifood/src/app/features/auth/presentation/pages/register_page.dart';
-import 'package:planning_poker_ifood/src/app/features/home/presentation/pages/home_page.dart';
+import 'package:planning_poker_ifood/src/app/features/room/presentation/pages/create_room_page.dart';
+import 'package:planning_poker_ifood/src/app/features/room/presentation/pages/room_page.dart';
 import 'package:planning_poker_ifood/src/core/features/splash/pages/splash_page.dart';
 
 final router = GoRouter(
@@ -13,6 +14,12 @@ final router = GoRouter(
     ),
     GoRoute(path: '/login', builder: (context, state) => const LoginPage()),
     GoRoute(path: '/register', builder: (context, state) => const RegisterPage()),
-    GoRoute(path: '/home', builder: (context, state) => const HomePage()),
+    GoRoute(
+      path: '/room',
+      builder: (context, state) => const RoomPage(),
+      routes: [
+        GoRoute(path: 'create-room', builder: (context, state) => const CreateRoomPage()),
+      ],
+    ),
   ],
 );
