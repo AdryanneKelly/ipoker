ec9a11334d9a2cf72cbd3440e7e462d72f0c4f9e
Adryanne Kelly <adryannekelly123@gmail.com>
2024-11-17 17:49:45 -0300

    feat: injecting new dependences

--- lib/src/core/DI/dependence_injector.dart
+++ lib/src/core/DI/dependence_injector.dart
@@ -7,6 +7,19 @@ import 'package:planning_poker_ifood/src/app/features/auth/domain/repositories/a
 import 'package:planning_poker_ifood/src/app/features/auth/domain/usecases/login_usecase.dart';
 import 'package:planning_poker_ifood/src/app/features/auth/domain/usecases/register_usecase.dart';
 import 'package:planning_poker_ifood/src/app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:planning_poker_ifood/src/app/features/room/data/datasources/room_remote_datasource.dart';
+import 'package:planning_poker_ifood/src/app/features/room/data/repositories/room_repository_impl.dart';
+import 'package:planning_poker_ifood/src/app/features/room/domain/repositories/room_repository_interface.dart';
+import 'package:planning_poker_ifood/src/app/features/room/domain/usecases/create_room_usecase.dart';
+import 'package:planning_poker_ifood/src/app/features/room/domain/usecases/create_task_usecase.dart';
+import 'package:planning_poker_ifood/src/app/features/room/domain/usecases/get_room_usecase.dart';
+import 'package:planning_poker_ifood/src/app/features/room/presentation/bloc/room_bloc.dart';
+import 'package:planning_poker_ifood/src/core/features/user/data/datasources/user_remote_datasource.dart';
+import 'package:planning_poker_ifood/src/core/features/user/data/repositories/user_repository_impl.dart';
+import 'package:planning_poker_ifood/src/core/features/user/domain/repositories/user_repository_interface.dart';
+import 'package:planning_poker_ifood/src/core/features/user/domain/usecases/get_user_usecase.dart';
+import 'package:planning_poker_ifood/src/core/features/user/domain/usecases/register_user_usecase.dart';
+import 'package:planning_poker_ifood/src/core/features/user/presentation/bloc/user_bloc.dart';
 
 final injector = GetIt.instance;
 FirebaseAuth get firebaseAuth => FirebaseAuth.instance;
@@ -21,4 +34,21 @@ void setupDI() {
   injector.registerFactory<LoginUsecase>(() => LoginUsecase(authRepository: injector<IAuthRepository>()));
   injector.registerFactory<RegisterUsecase>(() => RegisterUsecase(authRepository: injector<IAuthRepository>()));
   injector.registerLazySingleton<AuthBloc>(() => AuthBloc(loginUsecase: injector<LoginUsecase>(), registerUsecase: injector<RegisterUsecase>()));
+  injector.registerFactory<RoomRemoteDatasource>(
+      () => RoomRemoteDatasource(firebaseFirestore: injector<FirebaseFirestore>()));
+  injector.registerFactory<IRoomRepository>(() => RoomRepositoryImpl(roomDatasource: injector<RoomRemoteDatasource>()));
+  injector.registerFactory<GetRoomUsecase>(() => GetRoomUsecase(roomRepository: injector<IRoomRepository>()));
+  injector.registerFactory<CreateRoomUsecase>(() => CreateRoomUsecase(roomRepository: injector<IRoomRepository>()));
+  injector.registerFactory<CreateTaskUsecase>(() => CreateTaskUsecase(roomRepository: injector<IRoomRepository>()));
+  injector.registerLazySingleton<RoomBloc>(() => RoomBloc(
+      getRoomUsecase: injector<GetRoomUsecase>(),
+      createRoomUsecase: injector<CreateRoomUsecase>(),
+      createTaskUsecase: injector<CreateTaskUsecase>()));
+  injector.registerFactory<UserRemoteDatasource>(
+      () => UserRemoteDatasource(firebaseFirestore: injector<FirebaseFirestore>()));
+  injector.registerFactory<IUserRepository>(
+      () => UserRepositoryImpl(userRemoteDatasource: injector<UserRemoteDatasource>()));
+  injector.registerFactory<GetUserUsecase>(() => GetUserUsecase(repository: injector<IUserRepository>()));
+  injector.registerFactory<RegisterUserUsecase>(() => RegisterUserUsecase(repository: injector<IUserRepository>()));
+  injector.registerLazySingleton(() => UserBloc(getUserUsecase: injector<GetUserUsecase>()));
 }
