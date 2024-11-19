5a7076e4f0fc2387e8afdb13503f791ffb3632ae
Adryanne Kelly <adryannekelly123@gmail.com>
2024-11-19 13:49:14 -0300

    style: new login and register page layout

--- lib/src/app/features/auth/presentation/pages/login_page.dart
+++ lib/src/app/features/auth/presentation/pages/login_page.dart
@@ -19,98 +19,105 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final authBloc = context.read<AuthBloc>();
+    final theme = Theme.of(context);
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/logo_ipoker.png', width: 200),
-                const SizedBox(height: 20),
-                Text('Entre na sua conta', style: GoogleFonts.nunito(fontSize: 20, fontWeight: FontWeight.bold)),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: emailController,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Campo obrigatório';
-                    } else if (!value.contains('@')) {
-                      return 'Email inválido';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/images/background_image.webp',
+            ),
+            fit: BoxFit.fitWidth,
+            alignment: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/logo_ipoker.png', width: 200),
+                  const SizedBox(height: 20),
+                  Text('Entre na sua conta', style: theme.textTheme.titleLarge),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: emailController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Campo obrigatório';
+                      } else if (!value.contains('@')) {
+                        return 'Email inválido';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: passwordController,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Campo obrigatório';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    labelText: 'Senha',
-                    border: OutlineInputBorder(),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Campo obrigatório';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Senha',
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                BlocConsumer<AuthBloc, AuthState>(
-                  listener: (context, state) {
-                    if (state is LoginAuthFailure) {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          backgroundColor: Colors.red,
-                          content: Text(
-                            state.message,
-                            style: GoogleFonts.nunito(color: Colors.white),
-                          )));
-                    }
-                    if (state is LoginAuthSuccess) {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          backgroundColor: Colors.green,
-                          content: Text(
-                            'Login realizado com sucesso',
-                            style: GoogleFonts.nunito(color: Colors.white),
-                          )));
-                      router.go('/home');
-                    }
-                  },
-                  builder: (context, state) {
-                    if (state is LoginAuthLoading) {
-                      return const CircularProgressIndicator();
-                    }
-                    return ElevatedButton(
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          authBloc.add(LoginAuthEvent(
-                            email: emailController.text.trim(),
-                            password: passwordController.text.trim(),
-                          ));
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(double.infinity, 50),
-                        backgroundColor: Colors.red,
-                      ),
-                      child: const Text('Entrar', style: TextStyle(fontSize: 16, color: Colors.white)),
-                    );
-                  },
-                ),
-                const SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {
-                    router.go('/register');
-                  },
-                  child: const Text('Não tem uma conta? Cadastre-se', style: TextStyle(fontSize: 16)),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  BlocConsumer<AuthBloc, AuthState>(
+                    listener: (context, state) {
+                      if (state is LoginAuthFailure) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            backgroundColor: Colors.red,
+                            content: Text(
+                              state.message,
+                              style: theme.textTheme.displayLarge,
+                            )));
+                      }
+                      if (state is LoginAuthSuccess) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            backgroundColor: Colors.green,
+                            content: Text(
+                              'Login realizado com sucesso',
+                              style: GoogleFonts.nunito(color: Colors.white),
+                            )));
+                        router.go('/room');
+                      }
+                    },
+                    builder: (context, state) {
+                      if (state is LoginAuthLoading) {
+                        return const CircularProgressIndicator();
+                      }
+                      return ElevatedButton(
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            authBloc.add(LoginAuthEvent(
+                              email: emailController.text.trim(),
+                              password: passwordController.text.trim(),
+                            ));
+                          }
+                        },
+                        child: const Text('Entrar'),
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  TextButton(
+                    onPressed: () {
+                      router.go('/register');
+                    },
+                    child: const Text('Não tem uma conta? Cadastre-se'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/src/app/features/auth/presentation/pages/register_page.dart
+++ lib/src/app/features/auth/presentation/pages/register_page.dart
@@ -20,113 +20,125 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     final authBloc = context.read<AuthBloc>();
+    final theme = Theme.of(context);
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/logo_ipoker.png', width: 200),
-                const SizedBox(height: 20),
-                Text('Cadastre-se', style: GoogleFonts.nunito(fontSize: 20, fontWeight: FontWeight.bold)),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: nameController,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Campo obrigatório';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    labelText: 'Nome',
-                    border: OutlineInputBorder(),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/images/background_image.webp',
+            ),
+            fit: BoxFit.fitWidth,
+            alignment: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/logo_ipoker.png', width: 200),
+                  const SizedBox(height: 20),
+                  Text('Cadastre-se', style: theme.textTheme.titleLarge),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: nameController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Campo obrigatório';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Nome',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: emailController,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Campo obrigatório';
-                    } else if (!value.contains('@')) {
-                      return 'Email inválido';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: emailController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Campo obrigatório';
+                      } else if (!value.contains('@')) {
+                        return 'Email inválido';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: passwordController,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Campo obrigatório';
-                    } else if (value.length < 6 && !value.contains(RegExp(r'[0-9]'))) {
-                      return 'Senha deve ter no mínimo 6 caracteres, com letras e números';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    labelText: 'Senha',
-                    border: OutlineInputBorder(),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: passwordController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Campo obrigatório';
+                      } else if (value.length < 6 && !value.contains(RegExp(r'[0-9]'))) {
+                        return 'Senha deve ter no mínimo 6 caracteres, com letras e números';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Senha',
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                BlocConsumer<AuthBloc, AuthState>(
-                  listener: (context, state) {
-                    if (state is RegisterAuthFailure) {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          backgroundColor: Colors.red,
-                          content: Text(
-                            state.message,
-                            style: GoogleFonts.nunito(color: Colors.white),
-                          )));
-                    }
-                    if (state is RegisterAuthSuccess) {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          backgroundColor: Colors.green,
-                          content: Text(
-                            'Usuário cadastrado com sucesso',
-                            style: GoogleFonts.nunito(color: Colors.white),
-                          )));
-                      router.go('/home');
-                    }
-                  },
-                  builder: (context, state) {
-                    if (state is RegisterAuthLoading) {
-                      return const CircularProgressIndicator();
-                    }
-                    return ElevatedButton(
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          authBloc.add(RegisterAuthEvent(
-                            name: nameController.text.trim(),
-                            email: emailController.text.trim(),
-                            password: passwordController.text.trim(),
-                          ));
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(double.infinity, 50),
-                        backgroundColor: Colors.red,
-                      ),
-                      child: const Text('Cadastrar', style: TextStyle(fontSize: 16, color: Colors.white)),
-                    );
-                  },
-                ),
-                const SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text('Já tem uma conta? Faça login', style: TextStyle(fontSize: 16)),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  BlocConsumer<AuthBloc, AuthState>(
+                    listener: (context, state) {
+                      if (state is RegisterAuthFailure) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            backgroundColor: Colors.red,
+                            content: Text(
+                              state.message,
+                              style: GoogleFonts.nunito(color: Colors.white),
+                            ),
+                          ),
+                        );
+                      }
+                      if (state is RegisterAuthSuccess) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            backgroundColor: Colors.green,
+                            content: Text(
+                              'Usuário cadastrado com sucesso',
+                              style: GoogleFonts.nunito(color: Colors.white),
+                            )));
+                        router.go('/room');
+                      }
+                    },
+                    builder: (context, state) {
+                      if (state is RegisterAuthLoading) {
+                        return const CircularProgressIndicator();
+                      }
+                      return ElevatedButton(
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            authBloc.add(RegisterAuthEvent(
+                              name: nameController.text.trim(),
+                              email: emailController.text.trim(),
+                              password: passwordController.text.trim(),
+                            ));
+                          }
+                        },
+                        child: const Text('Cadastrar'),
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  TextButton(
+                    onPressed: () {
+                      router.go('/login');
+                    },
+                    child: const Text('Já tem uma conta? Faça login'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
