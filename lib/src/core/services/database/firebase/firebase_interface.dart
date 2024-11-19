2c3ff104c56449f4be24edd6abcd34f5fe6e121a
Adryanne Kelly <adryannekelly123@gmail.com>
2024-11-19 13:49:53 -0300

    feat: add getByUser function

--- lib/src/core/services/database/firebase/firebase_interface.dart
+++ lib/src/core/services/database/firebase/firebase_interface.dart
@@ -4,4 +4,5 @@ abstract interface class IFirebase {
   Future<void> update({required String collection, required String document, required Map<String, dynamic> data});
   Future<void> delete({required String collection, required String document});
   Future<Map<String, dynamic>> getByDocument({required String collection, required String document});
+  Future<List<Map<String, dynamic>>> getByUser({required String collection, required String userId});
 }
